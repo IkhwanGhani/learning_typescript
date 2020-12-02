d080708a05ac918609347b253aedde8eed88b75d
Ikhwan Ghani <IkhwanGhani@Mohamads-MacBook-Air.local>
2020-12-02 13:04:59 +0800

    2nd commit

---
+++ features/annotations/variables.ts
@@ -0,0 +1,13 @@
+let apples: number = 5;
+let speed: string = "fast";
+let hasName: boolean = true;
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// Built in Objects
+let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue']
+let myNumber: number[] = [1,2,3]
+let truths: boolean[] = [true, true, false]
