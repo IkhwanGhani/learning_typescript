638c4da4c1a4641585eb6dc44e738bc9a627b415
Ikhwan Ghani <IkhwanGhani@Mohamads-MacBook-Air.local>
2020-12-02 17:26:01 +0800

    3rd commit

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -11,3 +11,26 @@ let now: Date = new Date();
 let colors: string[] = ['red', 'green', 'blue']
 let myNumber: number[] = [1,2,3]
 let truths: boolean[] = [true, true, false]
+
+//Class
+class Car {
+
+}
+let car: Car = new Car();
+
+// Object literal
+let point: { x:number; y:number } = {
+  x: 10,
+  y: 20
+};
+
+// Function
+const logNumber: (i:number) => void = (i:number) => {
+  console.log(i)
+};
+
+// When to annotations
+// 1) Functions that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates = JSON.parse(json);
+console.log(coordinates);
